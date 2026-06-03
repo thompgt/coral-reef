04e842a195a0e49fadb4894f56089abe4e8310d3
Thomas Pequegnot <thomas.pequegnot04@gmail.com>
2026-06-02 21:16:13 -0400

    Cleanup: remove obsolete Django and DuckDB files

--- coral_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoralApiConfig(AppConfig):
-    name = 'coral_api'
--- coral_api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import SemanticSearchView
-
-urlpatterns = [
-    path('search/', SemanticSearchView.as_view(), name='semantic_search'),
-]
